830cbfb8692c6be749cdb477197535b11ff6c713
osaco <rikandrade0@gmail.com>
2024-09-07 17:43:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Raças de Gatos",
+        descricao: "Existem diversas raças de gatos, cada uma com suas características únicas de pelagem, cor dos olhos, tamanho e personalidade.Cada raça possui sua própria história e beleza, tornando a escolha do gatinho perfeito uma experiência incrível.",
+        link: "https://www.purina.pt/encontrar-animal/racas-gatos?page=%2C0"
+    },
+    {
+        titulo: "Cuidados com os Gatos",
+        descricao: "Para ter um gato feliz e saudável, é importante oferecer uma alimentação balanceada, água fresca, caixa de areia limpa, brinquedos e arranhadores. Além disso, é fundamental levá-lo ao veterinário regularmente para vacinação e check-ups.",
+        link: "https://petanjo.com/blog/cuidados-com-gatos-lista-de-cuidados-essenciais-que-todo-gato-precisa/"
+    },
+    {
+        titulo: "Preparativos para os Novos Inquilinos",
+        descricao: "Antes de trazer um gatinho para casa, prepare um ambiente seguro e confortável. Adquira todos os itens essenciais, como ração, tigelas, caixa de areia, brinquedos e um local tranquilo para ele dormir. Consulte um veterinário para obter orientações sobre a adaptação do gatinho ao novo lar.",
+        link: "https://love.doghero.com.br/gatos/adocao-de-gatos/"
+    }
+];
+
